52c8941beed885f80659b098c15efa338d830f61
tanmayhedau <tanmayhedau619@gmail.com>
2023-02-13 19:57:55 +0530

    useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import { func } from "prop-types";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 
 function App() {
+  const [state, setState] = useState(2);
+  const [data, setData] = useState([]);
 
-  const [num,setNum] = useState(1);
-  function inc(){
-    setNum(num+1)
-  }
+  useEffect(() => {
+    async function getData() {
+      const get = await fetch(
+        `https://hub.dummyapis.com/employee?noofRecords=${state}&idStarts=1001`
+      );
 
-  function dec(){
-    setNum(num-1)
-  }
+      const res = await get.json();
+      setData(res);
+      console.log(res);
+    }
+    getData();
+    document.title = `(${state}) Employees online`
+  }, [state]);
 
   return (
     <div className="App">
-      <Header /> 
-      <div className="main">
-        <h1 className="heading">{num}</h1>
-        <button className="btn" onClick={inc}>increment</button>
-        <button className="btn" onClick={dec}>decrement</button>
-      </div>
+      <Header />
+      <button onClick={() => setState(state + 1)}>Click me {state}</button>
+      {data.map((ele, index) => {
+        return (
+          <div className="data" key={index}>
+            <h4>{ele.firstName}</h4>
+            <h4>{ele.lastName}</h4>
+            <h4>{ele.email}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "../App.css";
 const Header = () => {
   return (
     <div className="header">
-      <h1>UseState Hook</h1>
+      <h1>UseEffect Hook</h1>
     </div>
   );
 };
