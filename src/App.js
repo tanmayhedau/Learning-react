dc0e56fa6f8f56ac84908f82322fa600394c73e1
tanmayhedau <tanmayhedau619@gmail.com>
2023-02-15 13:01:31 +0530

    useReducer Hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import { useState, useEffect, useRef } from "react";
+import { useReducer } from "react";
 import "./App.css";
 import Header from "./components/Header";
 
+const reducer = (state, action) => {
+  if (action.type === "INC") {
+    return state + 2;
+  } else if (action.type === "DEC") {
+    return state - 2;
+  } else if (action.type === "MUL") {
+    return state * 2;
+  }
+  return state;
+};
+
 function App() {
-  const inputField = useRef();
-
-  const clickHandler = () => {
-    console.log(inputField.current.value);
-    inputField.current.style.border = "2px solid red";
-    console.log(inputField.current);
-  };
-
+  const [state, dispatch] = useReducer(reducer, 0);
   return (
     <div className="App">
       <Header />
-      <input ref={inputField} />
-
-      <button onClick={clickHandler}>submit</button>
+      <h1>{state}</h1>
+      <button onClick={() => dispatch({ type: "INC" })}>increment</button>
+      <button onClick={() => dispatch({ type: "DEC" })}>decrement</button>
+      <button onClick={() => dispatch({ type: "MUL" })}>multipy</button>
     </div>
   );
 }
